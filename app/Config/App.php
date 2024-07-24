db3d68a96f4b27b3ff3fedf28cce776d1dd8adda
swaynz <55703949+nazirxz@users.noreply.github.com>
2024-07-24 20:48:29 +0700

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost:8080/';
+    public string $baseURL = 'http://prjctakhir-91b24c417509.herokuapp.com/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
