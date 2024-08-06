b1f7a89ffffee8a72b5fca84161b86d91e43ec0b
wilsonwijaya5 <85544392+wilsonwijaya5@users.noreply.github.com>
2024-08-06 20:46:43 +0700

    Update main.php

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sicepat Admin Dashboard</title>
+    <title>SiCepat | Layanan Kirim Paket Ekspress No.1 di Indonesia</title>
     
     <!-- CSS -->
     <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.css') ?>">
